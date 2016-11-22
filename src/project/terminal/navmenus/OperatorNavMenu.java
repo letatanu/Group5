f756e86beacff5d9a9e6f21737cfb2e3236045b4
anders-swanson <landersx@live.com>
2016-11-22 14:27:18 -0800

    Added some to navigation menus so that they connect to the add/edit/remove functionalities

--- src/project/terminal/navmenus/OperatorNavMenu.java
+++ src/project/terminal/navmenus/OperatorNavMenu.java
@@ -7,6 +7,8 @@ import project.terminal.TerminalMenu;
 /**
  * Created by Anders on 11/17/2016.
  * Base menu for Operators to navigate the ChocAn system.
+ * Handles the modify condition for members as both providers
+ * and managers have this functionality. 
  */
 abstract class OperatorNavMenu extends SelectionMenu {
     OperatorNavMenu(String name, String title, String body, ProfilesDatabase profile) {
